fcfbf48041d2f1e5f2526bc32cb72c2f074cb97f
Dereje Deressa <derejetadese@gmail.com>
2026-03-26 11:36:41 -0400

    Delete app/reviews/[slug] directory

--- app/reviews/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReviewPage() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Product Review</h1>
-      <p>This page will contain detailed product reviews.</p>
-    </div>
-  );
-}
